fb9ee1a73d594869839e611d21a2f001a34e5a30
Rocco Wu <wu.rocco@gmail.com>
2021-10-23 15:16:54 -0700

    Respawn Iteration
    - Created new Respawn with timers

--- Assets/Scripts/RespawnManager.cs
+++ Assets/Scripts/RespawnManager.cs
@@ -6,35 +6,65 @@ public class RespawnManager : MonoBehaviour
 {
     public Transform player1, player2, ball;
     public Transform p1Respawn, p2Respawn, ballRespawn;
+    private float respawnTimerP1 = 1.5f;
+    private float respawnTimerP2 = 1.5f;
+    public bool p1canRespawn = true;
+    public bool p2canRespawn = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (p1canRespawn)
+        {
+            player1.position = p1Respawn.position;
+            p1canRespawn = false;
+            print("respawn");
+        }
+
+        if (p2canRespawn)
+        {
+            player2.position = p2Respawn.position;
+            p2canRespawn = false;
+            print("respawn");
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player1")
         {
-            player1.position = p1Respawn.position; 
-            print("respawn");
+            StartCoroutine(respawnDelayPlayer1());
         }
 
         else if (other.tag == "Player2")
         {
-            player2.position = p2Respawn.position; 
-            print("respawn");
+            StartCoroutine(respawnDelayPlayer2());
         }
+    }
 
-        else if (other.tag == "Ball")
+    IEnumerator respawnDelayPlayer1()
+    {
+        while (respawnTimerP1 > 0)
         {
-            ball.position = ballRespawn.position; 
-            print("respawn");
+            yield return new WaitForSeconds(respawnTimerP1);
+            respawnTimerP1--;
         }
+        respawnTimerP1 = 0;
+        p1canRespawn = true;
+    }
+
+    IEnumerator respawnDelayPlayer2()
+    {
+        while (respawnTimerP2 > 0)
+        {
+            yield return new WaitForSeconds(respawnTimerP2);
+            respawnTimerP2--;
+        }
+        respawnTimerP2 = 0;
+        p2canRespawn = true;
     }
 }
