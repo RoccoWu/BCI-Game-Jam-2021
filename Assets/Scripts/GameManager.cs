8cec52ab77941d6962e81b1ac5b1d2ae9d21674d
Rocco Wu <wu.rocco@gmail.com>
2021-10-22 20:05:29 -0700

    Basic Movement
    - Created scripts for players
    - Players can now move and interact

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject respawnBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if players hit the respawn box, then respawn point
+    }
+}
---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public int speed;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+     void FixedUpdate()
+    {
+        //when player 2 presses [button] add negative force to Z axis
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.AddForce(new Vector3(0f,0f,speed *1));
+        }
+    }
+}
