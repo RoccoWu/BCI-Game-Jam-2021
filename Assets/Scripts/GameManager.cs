be35548f84fdfe1b9a458bb8060b3d4f518b3c0a
Rocco Wu <wu.rocco@gmail.com>
2021-10-23 17:53:37 -0700

    Animation Working
    - Player can now pull animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject respawnBox;
+    public GameObject respawnBox, player1, player2;
     public float player1Points, player2Points;
-    
+    public float startGametimer = 1f;
+    public bool gameStart = false;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(GameStart());
     }
 
     // Update is called once per frame
@@ -19,7 +21,24 @@ public class GameManager : MonoBehaviour
         //if players hit the respawn box, then respawn point
     }
 
-    
+    private void startGame()
+    {
+        player1.GetComponent<Animator>().SetBool("canPull", true);
+        player2.GetComponent<Animator>().SetBool("canPull", true);
+        Debug.Log("rope pulling anim");
+    }
+    IEnumerator GameStart()
+    {
+        while (startGametimer > 0)
+        {
+            yield return new WaitForSeconds(startGametimer);
+            startGametimer--;
+        }
+        startGametimer = 0;
+        Debug.Log("timer up");
+        startGame();
+    }
+
 }
 
 
