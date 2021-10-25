f73a6f3b0c94fa9969bc3e54bfb2e6d6032a6395
Rocco Wu <wu.rocco@gmail.com>
2021-10-24 20:06:01 -0700

    Fixed Bugs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Transform target;
+    public Transform target;
 
     [SerializeField]
     private Transform player1, player2;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-
+    void Start()
+    {
+        
+    }
     void FixedUpdate()
     {
         if (GameManager.instance.isGameScene())
@@ -16,11 +19,16 @@ public class CameraFollow : MonoBehaviour
             Vector3 interpolated = Vector3.Lerp(player1.position, player2.position, 0.5f);
             transform.position = new Vector3(
                 transform.position.x, transform.position.y, interpolated.z);
-            Vector3 desiredPosition = target.position + offset;
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-            transform.position = smoothedPosition;
 
-            transform.LookAt(target);
+            Vector3 desiredPosition = interpolated + offset;
+            if(target)
+            {
+                desiredPosition = target.position + offset;
+            }
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition +offset;
+
+            transform.LookAt(desiredPosition);
         }
     }
 
