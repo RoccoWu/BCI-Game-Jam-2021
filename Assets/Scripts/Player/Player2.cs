8f289a7eca0690998a6634b4f1cbb11c76bd6263
ekinney-lang <ekinneyL44@gmail.com>
2021-12-06 17:00:02 -0700

    Fixed
    Fixed main loop of the game. Now have properly - arrows indicating turns, math options selected appropriately, points added, movement of player 1/2.

--- Assets/Scripts/Player/Player2.cs
+++ Assets/Scripts/Player/Player2.cs
@@ -6,7 +6,7 @@ public class Player2 : MonoBehaviour
 {
     private Player2 instance;
      public int speed;
-      public string player2Choice = null;
+     public string player2Choice = null;
 
     [SerializeField]
     private Rigidbody rb;
@@ -17,35 +17,35 @@ public class Player2 : MonoBehaviour
         instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(GameManager.instance.player2Turn)      
-          //if player one presses a , they select answerchoice 1
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (GameManager.instance.player2Turn == true)
+    //        //if player one presses a , they select answerchoice 1
 
-          if(Input.GetKey(KeyCode.Keypad1))
-          {
-             player2Choice = GameManager.instance.answerChoice1.text; 
-          }
+    //        if (Input.GetKey(KeyCode.Alpha1) || Input.GetKey(KeyCode.Keypad1))// || Input.GetKey(KeyCode.LeftArrow))
+    //        {
+    //            player2Choice = GameManager.instance.answerChoice1.text;
+    //        }
 
-          else if(Input.GetKey(KeyCode.Keypad2))
-          {
-             player2Choice = GameManager.instance.answerChoice2.text; 
-          }    
-    }
+    //        else if (Input.GetKey(KeyCode.Alpha2) || Input.GetKey(KeyCode.Keypad2))// || Input.GetKey(KeyCode.RightArrow))
+    //        {
+    //            player2Choice = GameManager.instance.answerChoice2.text;
+    //        }
+    //}
 
-    void FixedUpdate()
-    {
-        //when player 2 presses [button] add negative force to Z axis
-        if(Input.GetKey(KeyCode.RightShift))
-        {
-            rb.AddForce(new Vector3(0f,0f,speed *-1));
-        }
+    //void FixedUpdate()
+    //{
+    //    //when player 2 presses [button] add negative force to Z axis
+    //    if(Input.GetKey(KeyCode.RightShift))
+    //    {
+    //        rb.AddForce(new Vector3(0f,0f,speed *-1));
+    //    }
 
-        else if(Input.GetKey(KeyCode.RightControl))
-        {
-            rb.AddForce(new Vector3(0f,0f,speed *1));
-        }
-    }
+    //    else if(Input.GetKey(KeyCode.RightControl))
+    //    {
+    //        rb.AddForce(new Vector3(0f,0f,speed *1));
+    //    }
+    //}
     
 }
